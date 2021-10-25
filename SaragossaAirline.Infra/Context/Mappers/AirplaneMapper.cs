7b48188ea7f51cd226ad3d3b842edb2d59e17687
Angel Gomez <agomez@gteam.es>
2021-10-25 11:30:39 +0200

    Context filled with mappers and DbSets. Fixed namespaces

--- SaragossaAirline.Infra/Context/Mappers/AirplaneMapper.cs
+++ SaragossaAirline.Infra/Context/Mappers/AirplaneMapper.cs
@@ -1,7 +1,7 @@
 ﻿using SaragossaAirline.Domain.Models;
 using System.Data.Entity.ModelConfiguration;
 
-namespace SaragossaAirline.Infra.Context {
+namespace SaragossaAirline.Infra.Context.Mappers {
     public class AirplaneMapper : EntityTypeConfiguration<AirplaneDTO> {
         public AirplaneMapper() {
             HasKey(i => i.Id);
--- SaragossaAirline.Infra/Context/ZaragozaAirlinesContext.cs
+++ SaragossaAirline.Infra/Context/ZaragozaAirlinesContext.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SaragossaAirline.Domain.Models;
+using SaragossaAirline.Infra.Context.Mappers;
+using System.Data.Entity;
 
 namespace SaragossaAirline.Infra.Context {
-    public class ZaragozaAirlinesContext {
+    public class ZaragozaAirlinesContext : DbContext {
+        public ZaragozaAirlinesContext() : base(@"Data Source=(localdb)\AirLines;Initial Catalog=ZaragozaAirlines;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False") {
+        }
+        public DbSet<AirlineDTO> Airlines { get; set; }
+        public DbSet<AirplaneDTO> Airplanes { get; set; }
+        public DbSet<InvoicingDTO> Invoicings { get; set; }
+        public DbSet<PassengerDTO> Passengers { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            modelBuilder.Configurations.Add(new AirplaneMapper());
+            modelBuilder.Configurations.Add(new AirlineMapper());
+            modelBuilder.Configurations.Add(new InvoicingMapper());
+            modelBuilder.Configurations.Add(new PassengerMapper());
 
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
